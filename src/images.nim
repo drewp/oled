0ef9b176a32ccfa76309c1d2aa8284ac788ccc7e
Drew Perttula <drewp@bigasterisk.com>
2018-04-07 02:54:35 -0700

    fix image readBytes

--- src/images.nim
+++ src/images.nim
@@ -14,10 +14,9 @@ proc newImages*(dir: Filename): Images =
 
 proc getPixelColumn*(this: Images, path: Filename,
                      x, y, h: int): seq[Color] =
-
   let fw = 150
   let fh = 8
-  echo &"get at {x} {y} {h}"
+
   let
     x = x.clamp(0, fw - 1)
     y = y.clamp(0, fh - h.clamp(0, fh))
@@ -28,20 +27,15 @@ proc getPixelColumn*(this: Images, path: Filename,
 
   let pos = (fh * x + y) * 3
   f.setFilePos(pos)
-  echo "read at ", pos
-  var buf = alloc(h * 3 + 1)
-  let nread = f.readBuffer(buf, h * 3)
+
+  var buf = newSeq[uint8](h * 3)
+  let nread = f.readBytes(buf, 0, h * 3)
   if nread != h * 3:
     raise newException(IOError, $(path))
-  let bufInts = cast[seq[uint8]](buf)
-  #bufInts.setLen(h * 3)
-  #echo &"bufInts = {bufInts}"
+
   result = newSeq[Color](h)
   for i in 0..<h:
-    result[i] = rgb(bufInts[i * 3 + 0],
-                    bufInts[i * 3 + 1],
-                    bufInts[i * 3 + 2])
-    echo &"build result[{i}] from {bufInts[i * 3 + 0]} {result[i]}"
-  
-  dealloc(buf)
-  echo &"returning {result.len} of h={h}"
+    result[i] = rgb(buf[i * 3 + 0],
+                    buf[i * 3 + 1],
+                    buf[i * 3 + 2])
+
